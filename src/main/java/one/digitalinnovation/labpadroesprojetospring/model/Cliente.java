e1823d11ca2acdb28c06bc9947a183fe586646b3
Felipe França <alexandre.b.franca@gmail.com>
2023-03-31 17:22:07 -0300

    Entrega final.

---
+++ src/main/java/one/digitalinnovation/labpadroesprojetospring/model/Cliente.java
@@ -0,0 +1,43 @@
+package one.digitalinnovation.labpadroesprojetospring.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String nome;
+    @ManyToOne
+    private Endereco endereco;
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Endereco getEndereco() {
+        return this.endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+}
